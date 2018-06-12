72b0a075409e8987f74ff9ffd05f7bfe8f777271
Martin Brugnara <martin.brugnara@gmail.com>
2018-06-12 23:07:00 +0200

    Setup data double bounding:w

--- app/static/js/structure.js
+++ app/static/js/structure.js
@@ -14,39 +14,47 @@ Vue.component('criterio', {
   },
 })
 
+
+/* Remember: JavaScript is call-by-sharing.  Like by value,
+ *	   but for complex types you can use only pointers (thus copy its address).
+ * https://stackoverflow.com/questions/518000/is-javascript-a-pass-by-reference-or-pass-by-value-language#3638034
+ */
+current_bando = {
+	criteri: [
+		{
+			nome:'A',
+			subcriteri: [
+				{nome:'A.1'},
+				{nome:'A.2'},
+				{nome:'A.3'},
+				{nome:'A.4', subcriteri:[{nome:'A.4.I'}]},
+			],
+		}, {
+			nome:'B',
+			subcriteri: [
+				{nome:'B.1'},
+				{
+					nome:'B.2',
+					subcriteri:[
+						{nome:'B.2.I'},
+						{nome:'B.2.II'},
+					]
+				},
+				{nome:'B.3'},
+			],
+		}, {
+			nome:'C',
+			test: "bla",
+		}
+	]
+};
+
+
 /* NOTE: refer to this example (contains addition, deletion, ...).
  * https://vuejs.org/v2/examples/tree-view.html */
 new Vue({
     el: '#tech_lst',
-    data: {
-        criteri: [
-            {
-                nome:'A',
-                subcriteri: [
-                    {nome:'A.1'},
-                    {nome:'A.2'},
-                    {nome:'A.3'},
-                    {nome:'A.4', subcriteri:[{nome:'A.4.I'}]},
-                ],
-            }, {
-                nome:'B',
-                subcriteri: [
-                    {nome:'B.1'},
-                    {
-                        nome:'B.2',
-                        subcriteri:[
-                            {nome:'B.2.I'},
-                            {nome:'B.2.II'},
-                        ]
-                    },
-                    {nome:'B.3'},
-                ],
-            }, {
-                nome:'C',
-                test: "bla",
-            }
-        ]
-    },
+    data: current_bando,
 });
 
 //var tech_list = new Vue({

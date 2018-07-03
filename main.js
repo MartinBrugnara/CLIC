3eabbacf07bebcd1ca2ba7cfb9a3470d8671fb4b
Martin Brugnara <martin.brugnara@gmail.com>
2018-07-03 12:19:39 +0200

    Project refactor. Add GPL

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+/*
+CLIC what if?
+Esplora i metodi di aggiudicazione dell'offerta economicamente piu' vantaggiosa
+Copyright (C) 2018 Martin Brugnara - Università degli Studi di Trento
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+
+
 const {app, BrowserWindow, Menu, MenuItem, globalShortcut} = require('electron')
 const fs = require('fs')
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,22 @@
+/*
+CLIC what if?
+Esplora i metodi di aggiudicazione dell'offerta economicamente piu' vantaggiosa
+Copyright (C) 2018 Martin Brugnara - Università degli Studi di Trento
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const {ipcRenderer} = require('electron')
 const {dialog} = require('electron').remote
 const fs = require('fs')
@@ -31,6 +50,8 @@ const path = require('path')
 // Extra)  Aggiungere funzione IDENTITA' che puo' essere usata solo con electre
 //  (non richiede scalare tra 0-1)
 //
+//
+//  *) TEST AGAINS https://decision-radar.com/
 //
 //
 //  ASSUMPTION:
